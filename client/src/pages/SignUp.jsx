a340037da0eeac461e2454d7785a86acb5c5c298
rahulhpujari <rahulhpujari@gmail.com>
2024-04-10 23:07:16 +0530

    add redux toolkit

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice.js'
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
