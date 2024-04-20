5e11d75c9426d55f123b08df8b885dce662a95f5
rahulhpujari <rahulhpujari@gmail.com>
2024-04-20 19:45:13 +0530

    complete create listing page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice.js'
-import OAuth from '../components/OAuth.jsx';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
